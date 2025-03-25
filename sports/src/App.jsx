5223dce0536ddcf224bdf9eef45caefe8fc79f59
romainkevin02 <romainkevinm@gmail.com>
2025-03-25 09:42:13 +0200

    this is todays task 25-03

---
+++ sports/src/App.jsx
@@ -0,0 +1,16 @@
+import MainBody from "./comps/body"
+import NavBar from "./comps/navBar"
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <MainBody/>
+      
+    </>
+  )
+}
+
+export default App
